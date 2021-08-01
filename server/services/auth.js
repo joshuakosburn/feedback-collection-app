7938fa41216006feab221a2de76685d76cca1508
Joshua Osburn <joshuaosburn@Joshuas-MacBook-Pro.local>
2021-07-31 21:30:53 -0400

    Fixed auth URLs

--- server/services/auth.js
+++ server/services/auth.js
@@ -23,7 +23,7 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/login/auth/google/callback',
+        callbackURL: '/auth/google/callback',
         proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
